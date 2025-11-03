ca631acbb7ea917d5eac728ad737b22a70dc9fee
OkuhleM <lindokuhlea38@gmail.com>
2025-11-03 23:19:03 +0200

    add billings table

--- Backend/Controllers/BillingController.js
+++ Backend/Controllers/BillingController.js
@@ -16,11 +16,13 @@ const patientsBillings = async (req, res) => {
       where: { id_Number: `${id_Number}` },
     });
 
-    const medicalAidFound = await medicalAid.findOne({
-      where: { medical_aid_id: `${medical_aid_id}` },
-    });
-    if (!medicalAid) {
-      return res.status(404).json({ message: "medical aid not found" });
+    let medicalAidFound = null;
+
+    if (!medical_aid_id) {
+      medicalAidFound = await medicalAid.findOne({ where: { medical_aid_id } });
+      if (!medicalAidFound) {
+        return res.status(404).json({ message: "medical aid not found" });
+      }
     }
     if (!patient) {
       return res.status(404).json({ message: "patient not found" });
@@ -44,11 +46,22 @@ const patientsBillings = async (req, res) => {
 
 const getAllAccounts = async (req, res) => {
   try {
-    const accounts = await Billings.findAll();
+    const accounts = await Billings.findAll({
+      include: [
+        {
+          model: PatientModel,
+          attributes: ["first_name", "last_name", "id_Number"],
+        },
+        {
+          model: medicalAid,
+          attributes: ["medical_aid_name", "policy_number"],
+        },
+      ],
+    });
 
     console.log("accounts", accounts);
 
-    res.status(200).json({ message: 'accounts: ',accounts});
+    res.status(200).json({ message: "accounts: ", data: accounts });
   } catch (error) {
     console.error("error", error);
     res.status(500).json({ message: "error finding accouts", error });
@@ -57,12 +70,29 @@ const getAllAccounts = async (req, res) => {
 
 const getAPatientsAccount = async (req, res) => {
   try {
-    const { billing_id } = req.params;
+    const { bill_id } = req.params;
     const foundBill = await Billings.findOne({
-      billing_id: `${billing_id}`,
+      where: { bill_id },
+      include: [
+        {
+          model: PatientModel,
+          attributes: ["first_name", "last_name", "id_Number"],
+        },
+        {
+          model: medicalAid,
+          attributes: ["medical_aid_name", "policy_number"],
+        },
+      ],
     });
+
+    if (!foundBill) {
+      return res.status(404).json({
+        message: "Billing record retrieved successfully",
+        data: foundBill,
+      });
+    }
     console.log(foundBill);
-    res.status(200).json({message: 'available bills', foundBill });
+    res.status(200).json({ message: "available bills", foundBill });
   } catch (error) {
     console.error("error", error);
     res.status(500).json({ message: "error finding foundBill", error });
